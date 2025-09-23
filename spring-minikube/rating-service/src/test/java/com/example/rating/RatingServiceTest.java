903b44e8c8923b6f1e2d0d8c7394b999432d92c0
anitha <anithaannem852@gmail.com>
2025-09-24 00:55:29 +0530

    commited

---
+++ spring-minikube/rating-service/src/test/java/com/example/rating/RatingServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.rating;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RatingServiceTest {
+
+    @Test
+    void testTrue() {
+        assertTrue(true, "This test always passes");
+    }
+}
