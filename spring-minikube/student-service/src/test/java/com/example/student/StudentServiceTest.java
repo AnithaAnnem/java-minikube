218e11da6105a1d7d8bbe3f773018271a81e9b09
anitha <anithaannem852@gmail.com>
2025-09-24 00:57:23 +0530

    commited

---
+++ spring-minikube/student-service/src/test/java/com/example/student/StudentServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.student;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StudentServiceTest {
+
+    @Test
+    void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result, "Simple addition should work");
+    }
+}
