d41d53604a61328c6d74dac9cda40ccdb0a4859f
AnithaAnnem <anitha.annem.snaatak@mygurukulam.co>
2025-09-26 17:13:29 +0530

    Update StudentController.java

--- spring-minikube/student-service/src/main/java/example/StudentController.java
+++ spring-minikube/student-service/src/main/java/example/StudentController.java
@@ -1,12 +1,14 @@
 package example;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequestMapping("/student")  // prefix all endpoints
 public class StudentController {
 
-    @GetMapping("/")   // root path for Ingress
+    @GetMapping("/")
     public String home() {
         return "Student Service Home";
     }
