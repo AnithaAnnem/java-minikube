7d65a5ffbf5b0be8a31d0bc4b564ff6bc114c03a
AnithaAnnem <anitha.annem.snaatak@mygurukulam.co>
2025-09-26 10:08:18 +0530

    Update RatingController.java

--- spring-minikube/rating-service/src/main/java/example/RatingController.java
+++ spring-minikube/rating-service/src/main/java/example/RatingController.java
@@ -1,18 +1,17 @@
 package example;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class RatingController {
 
-    @RequestMapping(value = "/check")
-    public String hi() {
-        return "Checking Rating";
+    @GetMapping("/")   // root path
+    public String home() {
+        return "Rating Service Home";
     }
 
-    @GetMapping(value = "/action")
+    @GetMapping("/action")
     public String action() {
         return "Rating Action";
     }
