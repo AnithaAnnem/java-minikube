20ca3ee39f043c96f8c9182f1573268be53480c0
AnithaAnnem <anitha.annem.snaatak@mygurukulam.co>
2025-09-26 17:14:17 +0530

    Update RatingController.java

--- spring-minikube/rating-service/src/main/java/example/RatingController.java
+++ spring-minikube/rating-service/src/main/java/example/RatingController.java
@@ -1,12 +1,14 @@
 package example;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequestMapping("/rating")  // Prefix all endpoints with /rating
 public class RatingController {
 
-    @GetMapping("/")   // root path for Ingress
+    @GetMapping("/")
     public String home() {
         return "Rating Service Home";
     }
